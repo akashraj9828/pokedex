a12d1bf5ef1b3461cb353fc1d85aaf64f35db468
Akash Raj <akashraj9828@gmail.com>
2025-08-10 20:30:13 +0530

    build fix maybe

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,39 +1,30 @@
 import '@/assets/styles/globals.css'
-import type { AppProps } from 'next/app'
-import { DeviceProvider } from '@/context/DeviceContext'
-import MainLayout from '@/layout/MainLayout'
-import { Provider } from 'react-redux'
-import { useStore } from '@/reducers/store'
-import { useEffect } from 'react'
-import {
-  fetchPokemonByName,
-  fetchPokemonEvolutionById,
-  fetchPokemonList,
-  fetchPokemonSpeciesByName,
-  fetchPokemonTypesList,
-} from '@/helpers/pokedex_api'
 import Meta from '@/components/seo/Meta'
 import { STRING } from '@/constants/string'
+import { DeviceProvider } from '@/context/DeviceContext'
+import { fetchPokemonList, fetchPokemonTypesList } from '@/helpers/pokedex_api'
+import MainLayout from '@/layout/MainLayout'
+import { useStore } from '@/reducers/store'
+import type { AppProps } from 'next/app'
+import { useEffect } from 'react'
+import { Provider as ReduxProvider } from 'react-redux'
 
 function MyApp({ Component, pageProps }: AppProps) {
   const store = useStore()
   useEffect(() => {
     store.dispatch(fetchPokemonTypesList() as any)
     store.dispatch(fetchPokemonList() as any)
-    // store.dispatch(fetchPokemonByName('eevee') as any)
-    // store.dispatch(fetchPokemonEvolutionById(133) as any)
-    // store.dispatch(fetchPokemonSpeciesByName('eevee') as any)
   }, [])
 
   return (
     <>
       <Meta title={`${STRING.POKEDEX} | Home`} />
       <DeviceProvider>
-        <Provider store={store}>
+        <ReduxProvider store={store} ref={null}>
           <MainLayout>
             <Component {...pageProps} />
           </MainLayout>
-        </Provider>
+        </ReduxProvider>
       </DeviceProvider>
     </>
   )
